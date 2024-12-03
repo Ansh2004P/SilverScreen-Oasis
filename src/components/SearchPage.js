9d1e4db9c5aa9d83765119398f190ad4d9515b59
Ansh2004P <anshkpatel15@gmail.com>
2024-12-04 00:46:25 +0530

    done

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -9,6 +9,12 @@ const SearchPage = () => {
       <div className="fixed -z-50 w-screen h-screen">
         <img className="object-cover" src={SEARCH_URL} alt={"Background"}></img>
       </div>
+
+      <span className="text-white text-2xl absolute my-32 mx-20">
+        Gpt search does not work due to openAI key expiry and will let to
+        breaking of website
+      </span>
+
       <SearchBar />
       {/* <GptKey /> */}
       <GptMovieSuggestions />
